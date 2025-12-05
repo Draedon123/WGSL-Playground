360516af52cf9cc70b42eebd2a790d446b620d34
Draedon <draedon123@gmail.com>
2025-12-05 23:30:47 +1000

    Hash based routing fix attempt 6

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,11 @@
   import { onMount } from "svelte";
 
   let redirect: HTMLAnchorElement;
+
   onMount(() => {
     const match = location.hash.match(/#redirect=.+$/);
 
     if (match === null || match.length === 0) {
-      redirect.remove();
       return;
     }
 
@@ -23,16 +23,11 @@
 </svelte:head>
 
 <main>
-  <a href="{resolve('/shaders')}#test1" data-sveltekit-reload>Test Shader 1</a>
+  <a href="{resolve('/shaders')}#test1">Test Shader 1</a>
   <br />
-  <a href="{resolve('/shaders')}#test2" data-sveltekit-reload>Test Shader 2</a>
+  <a href="{resolve('/shaders')}#test2">Test Shader 2</a>
 </main>
 
 <!-- svelte-ignore a11y_consider_explicit_label -->
 <!-- svelte-ignore a11y_missing_attribute -->
-<a
-  style="display: none;"
-  bind:this={redirect}
-  href={resolve("/")}
-  data-sveltekit-reload={false}
-></a>
+<a style="display: none;" bind:this={redirect} href={resolve("/")}></a>
