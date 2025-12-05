bafbd0db0b4d1300397decb56bfcf7f90c06b8bf
Draedon <draedon123@gmail.com>
2025-12-06 00:38:23 +1000

    Set title of shader editor page

--- src/routes/shaders/+page.svelte
+++ src/routes/shaders/+page.svelte
@@ -2,18 +2,23 @@
   import { resolve } from "$app/paths";
   import { onMount } from "svelte";
   import { liveQuery } from "dexie";
-  import { database } from "$lib/Database";
+  import { database, type Project } from "$lib/Database";
 
   let id = $state(1);
-  let project = liveQuery(() => database.projects.where({ id }).toArray());
+  let _project = liveQuery(() => database.projects.where({ id }).toArray());
+  let project = $derived(($_project?.[0] ?? null) as Project | null);
 
   onMount(() => {
     id = parseInt(location.hash.slice(1));
   });
 </script>
 
+<svelte:head>
+  <title>WGSL Playground - {project?.name ?? "Loading..."}</title>
+</svelte:head>
+
 <main>
-  <h1>{$project?.[0].name ?? "Loading..."}</h1>
+  <h1>{project?.name ?? "Loading..."}</h1>
 
   <a href={resolve("/")}>Home</a>
 </main>
