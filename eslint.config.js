faea090bd67913e0b0fec47180a6be1d31024138
Draedon <draedon123@gmail.com>
2025-12-05 22:16:32 +1000

    Allow `require` statements in .cjs files in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -42,6 +42,9 @@ export default defineConfig(
     languageOptions: {
       globals: { ...globals.node },
     },
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
   },
   {
     ignores: ["build/**/*"],
