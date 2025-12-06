abd37d90848a411936362155e2e1218efca6da9d
Draedon <draedon123@gmail.com>
2025-12-06 17:29:04 +1000

    Navigation bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { resolve } from "$app/paths";
   import { database } from "$lib/Database";
   import { onMount } from "svelte";
 
@@ -9,4 +10,54 @@
   });
 </script>
 
-{@render children?.()}
+<nav>
+  {#snippet Item(href: string, content: string)}
+    <a {href}>{content}</a>
+  {/snippet}
+
+  {@render Item(resolve("/shaders"), "Shaders")}
+  {@render Item(resolve("/"), "Home")}
+</nav>
+
+<div class="content">
+  {@render children?.()}
+</div>
+
+<style lang="scss">
+  $navigation-height: 2.5rem;
+
+  nav {
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    width: 100vw;
+    height: $navigation-height;
+    font-size: calc(0.7 * $navigation-height);
+    align-content: center;
+
+    background-color: #666;
+
+    a {
+      color: #fff;
+      text-decoration: none;
+      transition: color 0.3s;
+
+      margin: 0 1ch;
+      float: right;
+
+      // first child is on the right because of `float: right`
+      &:first-child {
+        margin-right: 1.5ch;
+      }
+
+      &:hover {
+        color: #c56dee;
+      }
+    }
+  }
+
+  .content {
+    margin-top: $navigation-height;
+  }
+</style>
